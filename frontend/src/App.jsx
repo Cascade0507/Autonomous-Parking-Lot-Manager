fd96a24fb41ee4fa4abfef14847ec1c355213026
Cascade0507 <gtejaspai@gmail.com>
2024-11-04 23:56:23 +0530

    add multiple routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 import CarAnimation from './CarAnimation'
 import './CarAnimation.css'
 import Navbar from './NavBar'
+import LoginPage from './LoginPage'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Car from './Car'
+import StudentDashboard from './StudentDashboard'
+
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const [x,setX] = useState([])
-
-  // const fecthAPI = async () => {
-  //   const response = await axios.get('http://localhost:8080/api/users')
-  //   setX(response.data.users)
-  // };
-
-  // useEffect( () => {
-  //   fecthAPI();
-  // },[]); 
 
   return (
-    <div className='app'>
+    <Router>
+      <div>
       <Navbar/>
-      <div className='content'>
-        <CarAnimation/>
+      <Routes>
+      <Route path='/' exact Component={LoginPage} />
+      <Route path='/manager' Component={CarAnimation}/>
+      <Route path='/student_dashboard/' element={<StudentDashboard />}/>
+      </Routes>
       </div>
-    </div>
+    </Router>
   )
 }
 
